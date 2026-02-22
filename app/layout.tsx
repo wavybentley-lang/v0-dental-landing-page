c92c4f8aeb1f17635940baaa339483483c48f0c3
wavybentley-lang <wavybentley@gmail.com>
2026-02-22 10:21:45 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
+import { SpeedInsights } from "@vercel/speed-insights/next" // Add this line!
 import './globals.css'
 
 const _geist = Geist({ subsets: ["latin"] });
